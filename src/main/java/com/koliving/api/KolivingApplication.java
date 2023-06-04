1a3b24c098ce76a133b1db0646677ed8db7ca26d
noah0504789 <58963724+noah0504789@users.noreply.github.com>
2023-06-05 01:39:07 +0900

    Configure Swagger (#3)
    * Add Swagger dependencies to build.gradle
    
    * Configure Swagger to application.yml
    
    * Configure Swagger to OpenApi

---
+++ src/main/java/com/koliving/api/KolivingApplication.java
@@ -0,0 +1,13 @@
+package com.koliving.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KolivingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KolivingApplication.class, args);
+    }
+
+}
