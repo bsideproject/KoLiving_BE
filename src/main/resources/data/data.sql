6ee3fb0d5c7f33397c78699261383b30d054945d
noah0504789 <noah0504@naver.com>
2023-06-05 11:53:32 +0900

    Init sql : languages table

---
+++ src/main/resources/data/data.sql
@@ -0,0 +1,8 @@
+INSERT INTO LANGUAGES (LOCALE, MESSAGE_KEY, MESSAGE_CONTENT) VALUES
+     ('en', 'home_welcome','Welcome'),
+     ('ko', 'home_welcome','환영합니다'),
+     ('en', 'auth_email_subject','Confirm your email'),
+     ('ko', 'auth_email_subject','이메일 인증을 완료하세요'),
+     ('en', 'auth_email_subtitle','Click the link below to proceed with authentication'),
+     ('ko', 'auth_email_subtitle','아래 링크를 클릭하셔서 인증을 진행하세요')
+;
