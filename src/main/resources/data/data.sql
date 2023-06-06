7a6beb29addd1e356276d69d80cfa71dd89fc02d
noah0504789 <noah0504@naver.com>
2023-06-07 00:55:17 +0900

    Add insert row

--- src/main/resources/data/data.sql
+++ src/main/resources/data/data.sql
@@ -4,5 +4,7 @@ INSERT INTO LANGUAGE (LOCALE, MESSAGE_KEY, MESSAGE_CONTENT) VALUES
      ('en', 'auth_email_subject','Confirm your email'),
      ('ko', 'auth_email_subject','이메일 인증을 완료하세요'),
      ('en', 'auth_email_subtitle','Click the link below to proceed with authentication'),
-     ('ko', 'auth_email_subtitle','아래 링크를 클릭하셔서 인증을 진행하세요')
+     ('ko', 'auth_email_subtitle','아래 링크를 클릭하셔서 인증을 진행하세요'),
+     ('en', 'email_duplication','This email already exists'),
+     ('ko', 'email_duplication','이미 존재하는 이메일입니다')
 ;
