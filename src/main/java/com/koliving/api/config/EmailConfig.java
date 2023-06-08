0a3d7f2fee38d54701ebf80050f26b242b83826d
noah0504789 <noah0504@naver.com>
2023-06-08 20:28:40 +0900

    Configure JavaMailSender

---
+++ src/main/java/com/koliving/api/config/EmailConfig.java
@@ -0,0 +1,32 @@
+package com.koliving.api.config;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Getter
+@Setter
+@Configuration
+@ConfigurationProperties(prefix = "spring.mail")
+public class EmailConfig {
+
+    private String host;
+    private int port;
+    private String username;
+    private String password;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+        return mailSender;
+    }
+}
