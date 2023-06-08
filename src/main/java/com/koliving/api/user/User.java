e9a5aaae93b240b72479689de588d7c2a4782068
noah0504789 <noah0504@naver.com>
2023-06-08 11:27:28 +0900

    Add User Entity

---
+++ src/main/java/com/koliving/api/user/User.java
@@ -0,0 +1,42 @@
+package com.koliving.api.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Id
+    private Integer id;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String description;
+
+    @CreationTimestamp
+    private LocalDateTime createdDate;
+
+    @UpdateTimestamp
+    private LocalDateTime lastModifiedDate;
+
+    @Builder
+    public User(String email) {
+        this.email = email;
+    }
+
+}
