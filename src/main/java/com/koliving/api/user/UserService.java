df1f844384fdef8417638f41131bb295b29e3a6b
noah0504789 <noah0504@naver.com>
2023-06-15 18:19:21 +0900

    Implement saveTokenAndSendEmail method

---
+++ src/main/java/com/koliving/api/user/UserService.java
@@ -0,0 +1,28 @@
+package com.koliving.api.user;
+
+import com.koliving.api.event.ConfirmationTokenCreatedEvent;
+import com.koliving.api.registeration.token.ConfirmationToken;
+import com.koliving.api.registeration.token.IConfirmationTokenService;
+import lombok.AllArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@AllArgsConstructor
+public class UserService implements IUserService {
+
+    private final IConfirmationTokenService confirmationTokenService;
+    private final ApplicationEventPublisher eventPublisher;
+
+    @Override
+    @Transactional
+    public void saveTokenAndSendEmail(String mail) {
+        ConfirmationToken confirmationToken = ConfirmationToken.builder()
+                .email(mail)
+                .build();
+
+        ConfirmationToken savedToken = confirmationTokenService.saveToken(confirmationToken);
+        eventPublisher.publishEvent(new ConfirmationTokenCreatedEvent(savedToken));
+    }
+}
