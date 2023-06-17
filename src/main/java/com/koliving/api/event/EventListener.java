03323d205dc238e887b4b3c733fdcf49f9807d30
noah0504789 <noah0504@naver.com>
2023-06-17 14:21:24 +0900

    Remove registeration directory

--- src/main/java/com/koliving/api/event/EventListener.java
+++ src/main/java/com/koliving/api/event/EventListener.java
@@ -1,6 +1,6 @@
 package com.koliving.api.event;
 
-import com.koliving.api.registeration.token.IConfirmationTokenService;
+import com.koliving.api.token.IConfirmationTokenService;
 import lombok.AllArgsConstructor;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
