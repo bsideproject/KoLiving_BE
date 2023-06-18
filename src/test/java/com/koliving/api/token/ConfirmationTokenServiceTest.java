49675bae2878f20470e7688e0ae246eadfbd505a
noah0504789 <noah0504@naver.com>
2023-06-18 20:07:07 +0900

    getToken_success(): existent token

--- src/test/java/com/koliving/api/token/ConfirmationTokenServiceTest.java
+++ src/test/java/com/koliving/api/token/ConfirmationTokenServiceTest.java
@@ -3,10 +3,19 @@ package com.koliving.api.token;
 import com.koliving.api.clock.IClock;
 import com.koliving.api.email.IEmailService;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class ConfirmationTokenServiceTest {
@@ -39,7 +48,23 @@ class ConfirmationTokenServiceTest {
     }
 
     @Test
-    void getToken() {
+    @DisplayName("getToken() : token 값에 따른 행이 존재함")
+    void getToken_success() {
+        String testMail = "from@test.com";
+
+        ConfirmationToken token = ConfirmationToken.builder()
+                .email(testMail)
+                .build();
+        String tokenValue = token.getToken();
+
+        when(confirmationTokenRepository.findByToken(tokenValue)).thenReturn(Optional.of(token));
+
+        Optional<ConfirmationToken> result = confirmationTokenService.getToken(token.getToken());
+
+        assertTrue(result.isPresent());
+        assertEquals(tokenValue, result.get().getToken());
+
+        verify(confirmationTokenRepository, times(1)).findByToken(tokenValue);
     }
 
     @Test
