3ece035c262d67a99de607874ea0369df3a8bf22
noah0504789 <noah0504@naver.com>
2023-06-20 21:58:55 +0900

    Implement UserDetail interface in User: enabled, locked

--- src/main/java/com/koliving/api/user/User.java
+++ src/main/java/com/koliving/api/user/User.java
@@ -11,13 +11,16 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
 
 @Getter
 @NoArgsConstructor
 @Entity
-public class User {
+public class User implements UserDetails {
 
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Id
@@ -35,6 +38,9 @@ public class User {
     @Enumerated(EnumType.STRING)
     private SignUpStatus signUpStatus;
 
+    private boolean bEnabled;
+    private boolean bLocked;
+
     @CreationTimestamp
     private LocalDateTime createdDate;
 
@@ -45,6 +51,39 @@ public class User {
     public User(String email) {
         this.email = email;
         this.signUpStatus = SignUpStatus.PASSWORD_VERIFICATION_PENDING;
+        this.bEnabled = true;
+        this.bLocked = false;
     }
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return bEnabled;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !bLocked;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        // TODO : need field related to user expiration
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        // TODO : need field related to password expiration
+        return true;
+    }
 }
