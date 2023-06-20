957ebb9a48e169aef21dfdac512c4abb4eaab75d
noah0504789 <noah0504@naver.com>
2023-06-20 21:16:46 +0900

    Register SecurityFilterChain bean

--- src/main/java/com/koliving/api/config/SecurityConfig.java
+++ src/main/java/com/koliving/api/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.koliving.api.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -21,5 +23,17 @@ public class SecurityConfig {
                         "/resources/**",
                         "/swagger-ui/**"
                 );
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        String rootPath = String.format("/api/%s", currentVersion);
+
+        http.authorizeRequests()
+                // TODO : Add more specific path (No need to authenticate)
+                .requestMatchers(rootPath + "/signup/**").permitAll()
+                .anyRequest().authenticated();
+
+        return http.build();
     }
 }
