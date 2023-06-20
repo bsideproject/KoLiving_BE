442bb7d390c8b5adcab6fdddbe55bdba9eef94db
noah0504789 <noah0504@naver.com>
2023-06-20 20:43:12 +0900

    Register WebSecurityCustomizer bean: except unnecessary authentication

--- src/main/java/com/koliving/api/config/SecurityConfig.java
+++ src/main/java/com/koliving/api/config/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.koliving.api.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 
 @Configuration
 public class SecurityConfig {
@@ -10,5 +12,14 @@ public class SecurityConfig {
 
     public SecurityConfig(@Value("${server.current-version:v1}") String currentVersion) {
         this.currentVersion = currentVersion;
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) ->
+                web.ignoring().requestMatchers(
+                        "/resources/**",
+                        "/swagger-ui/**"
+                );
     }
 }
