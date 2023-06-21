41c17524406eb15c30f875575eb931d10f145e32
noah0504789 <noah0504@naver.com>
2023-06-21 14:25:38 +0900

    Refactor: Add passwordEncoder dependency to SecurityConfig class

--- src/main/java/com/koliving/api/config/SecurityConfig.java
+++ src/main/java/com/koliving/api/config/SecurityConfig.java
@@ -7,18 +7,21 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     private final UserDetailsService userService;
+    private final PasswordEncoder passwordEncoder;
     private String currentVersion;
 
     public SecurityConfig(UserDetailsService userService,
+            PasswordEncoder passwordEncoder,
             @Value("${server.current-version:v1}") String currentVersion) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
         this.currentVersion = currentVersion;
     }
 
@@ -46,7 +49,7 @@ public class SecurityConfig {
     @Bean
     public DaoAuthenticationProvider daoAuthenticationProvider() {
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-        provider.setPasswordEncoder(bCryptPasswordEncoder());
+        provider.setPasswordEncoder(passwordEncoder);
         provider.setUserDetailsService(userService);
         return provider;
     }
