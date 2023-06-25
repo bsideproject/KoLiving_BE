7ba3cdbdeaa1077b0d29bc50afc724d1e8a75fb7
noah0504789 <noah0504@naver.com>
2023-06-25 18:30:55 +0900

    Feat: Add JwtAuthenticationFilter

---
+++ src/main/java/com/koliving/api/config/JwtAuthenticationFilter.java
@@ -0,0 +1,65 @@
+package com.koliving.api.config;
+
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    public static final String BEARER_PREFIX = "Bearer ";
+    private final JwtProvider jwtProvider;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = null;
+        try {
+            token = resolveToken(request);
+        } catch (RuntimeException e) {
+            setResponse(response, e.getMessage());
+        }
+
+        try {
+            jwtProvider.validateToken(token);
+        } catch (ExpiredJwtException | MalformedJwtException | SignatureException | UnsupportedJwtException e) {
+            setResponse(response, e.getMessage());
+        }
+
+        Authentication authentication = jwtProvider.getAuthentication(token);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (bearerToken == null) {
+            throw new RuntimeException("Authorization header not found");
+        }
+
+        if (!bearerToken.startsWith(BEARER_PREFIX)) {
+            throw new RuntimeException("Authorization header does not start with Bearer");
+        }
+
+        return bearerToken.substring(BEARER_PREFIX.length());
+    }
+
+    private void setResponse(HttpServletResponse response, String errorMessage) throws IOException {
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        response.getWriter().write(errorMessage);
+    }
+}
