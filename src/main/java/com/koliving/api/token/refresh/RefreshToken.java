b20b37f45584049076ccc3cf5f0aec5bad60941a
noah0504789 <noah0504@naver.com>
2023-06-28 15:29:02 +0900

    Refactor: Use builder pattern to assign JWT value to refresh token field

--- src/main/java/com/koliving/api/token/refresh/RefreshToken.java
+++ src/main/java/com/koliving/api/token/refresh/RefreshToken.java
@@ -5,8 +5,8 @@ import lombok.Getter;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.redis.core.RedisHash;
 
-@RedisHash(value = "RefreshToken", timeToLive = 60*60*24*14) // 2week
 @Getter
+@RedisHash(value = "RefreshToken", timeToLive = 60*60*24*14) // 2week
 public class RefreshToken {
 
     @Id
@@ -14,7 +14,8 @@ public class RefreshToken {
     private String refreshToken;
 
     @Builder
-    public RefreshToken(String email) {
+    public RefreshToken(String email, String refreshToken) {
         this.email = email;
+        this.refreshToken = refreshToken;   // jwt
     }
 }
