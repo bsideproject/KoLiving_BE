36a3680c04435833b1b248b494f8110fba2e715c
noah0504789 <noah0504@naver.com>
2023-06-28 15:43:19 +0900

    Feat: Implement extractEmail() in JwtService

--- src/main/java/com/koliving/api/token/JwtService.java
+++ src/main/java/com/koliving/api/token/JwtService.java
@@ -1,6 +1,9 @@
 package com.koliving.api.token;
 
 import com.koliving.api.token.refresh.RefreshTokenRepository;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import jakarta.xml.bind.DatatypeConverter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -23,7 +26,8 @@ public class JwtService implements IJwtService {
 
     @Override
     public String extractEmail(String token) {
-        return null;
+        Claims claim = getClaims(token);
+        return (String) claim.get("email");
     }
 
     @Override
@@ -34,5 +38,12 @@ public class JwtService implements IJwtService {
     @Override
     public String saveRefreshToken(String email, String newRefreshTokenValue) {
         return null;
+    }
+
+    private Claims getClaims(String token) {
+        return Jwts.parser()
+                .setSigningKey(DatatypeConverter.parseBase64Binary(jwtSecret))
+                .parseClaimsJws(token)
+                .getBody();
     }
 }
