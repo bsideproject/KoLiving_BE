b05addb0d8565f0974cbb7202f7b11d03f8be58b
noah0504789 <noah0504@naver.com>
2023-06-28 15:42:42 +0900

    Feat: Add JwtService

---
+++ src/main/java/com/koliving/api/token/JwtService.java
@@ -0,0 +1,38 @@
+package com.koliving.api.token;
+
+import com.koliving.api.token.refresh.RefreshTokenRepository;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtService implements IJwtService {
+
+    private String jwtSecret;
+    private final UserDetailsService userService;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public JwtService(@Value("${jwt.secret}") String jwtSecret,
+                      UserDetailsService userService,
+                      RefreshTokenRepository refreshTokenRepository) {
+        this.jwtSecret = jwtSecret;
+        this.userService = userService;
+        this.refreshTokenRepository = refreshTokenRepository;
+    }
+
+    @Override
+    public String extractEmail(String token) {
+        return null;
+    }
+
+    @Override
+    public Authentication getAuthentication(String accessToken) {
+        return null;
+    }
+
+    @Override
+    public String saveRefreshToken(String email, String newRefreshTokenValue) {
+        return null;
+    }
+}
