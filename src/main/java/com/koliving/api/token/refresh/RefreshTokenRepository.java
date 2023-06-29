0cdac61f0133428cda6a811768dad1a23b3f6c54
noah0504789 <noah0504@naver.com>
2023-06-29 21:31:55 +0900

    Refactor: Declare static variable of HashOperations and assign value after initializing bean

--- src/main/java/com/koliving/api/token/refresh/RefreshTokenRepository.java
+++ src/main/java/com/koliving/api/token/refresh/RefreshTokenRepository.java
@@ -1,5 +1,6 @@
 package com.koliving.api.token.refresh;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
@@ -13,6 +14,7 @@ public class RefreshTokenRepository {
     private final RedisTemplate redisTemplate;
     private final long RT_EXPIRATION_TIME;
     private final static String RT_HASH_KEY = "RefreshToken";
+    private static HashOperations<String, String, String> hashOperations;
 
     public RefreshTokenRepository(RedisTemplate redisTemplate,
                                   @Value("${jwt.refreshExpiration:30}") long RT_EXPIRATION_TIME) {
@@ -20,22 +22,23 @@ public class RefreshTokenRepository {
         this.RT_EXPIRATION_TIME = RT_EXPIRATION_TIME;
     }
 
-    public String save(final RefreshToken refreshToken) {
-        HashOperations<String, String, String> hashOperations = redisTemplate.opsForHash();
-        hashOperations.putIfAbsent(RT_HASH_KEY, refreshToken.getEmail(), refreshToken.getRefreshToken());
+    @PostConstruct
+    private void init() {
+        hashOperations = redisTemplate.opsForHash();
+    }
 
+    public String save(final RefreshToken refreshToken) {
+        hashOperations.putIfAbsent(RT_HASH_KEY, refreshToken.getEmail(), refreshToken.getRefreshToken());
         redisTemplate.expire(RT_HASH_KEY, RT_EXPIRATION_TIME, TimeUnit.DAYS);
 
         return refreshToken.getRefreshToken();
     }
 
     public boolean existByEmail(final String email) {
-        HashOperations<String, String, String> hashOperations = redisTemplate.opsForHash();
         return hashOperations.hasKey(RT_HASH_KEY, email);
     }
 
     public void delete(final String email) {
-        HashOperations<String, String, String> hashOperations = redisTemplate.opsForHash();
         hashOperations.delete(RT_HASH_KEY, email);
     }
 }
