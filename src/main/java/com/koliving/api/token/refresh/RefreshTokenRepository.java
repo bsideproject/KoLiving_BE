5ef6f69e6cc026db70c2a70c97a625eab12137e7
noah0504789 <noah0504@naver.com>
2023-06-29 17:16:28 +0900

    Refactor: Delete redis repository interface

--- src/main/java/com/koliving/api/token/refresh/RefreshTokenRepository.java
+++
@@ -1,6 +0,0 @@
-package com.koliving.api.token.refresh;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
-}
