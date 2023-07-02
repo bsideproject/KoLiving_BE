93dc982db899eb5fa4384c26353803e3de597df3
noah0504789 <noah0504@naver.com>
2023-07-02 20:16:12 +0900

    Feat: Migrate saveTokenAndSendEmail() from UserService to AuthFacade

---
+++ src/main/java/com/koliving/api/user/AuthFacade.java
@@ -0,0 +1,25 @@
+package com.koliving.api.user;
+
+import com.koliving.api.event.ConfirmationTokenCreatedEvent;
+import com.koliving.api.token.confirmation.ConfirmationToken;
+import com.koliving.api.token.confirmation.IConfirmationTokenService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class AuthFacade {
+    private final IConfirmationTokenService confirmationTokenService;
+    private final ApplicationEventPublisher eventPublisher;
+
+    public void saveTokenAndSendEmail(String email) {
+        ConfirmationToken newToken = confirmationTokenService.createToken(email);
+        ConfirmationToken savedToken = confirmationTokenService.saveToken(newToken);
+        eventPublisher.publishEvent(new ConfirmationTokenCreatedEvent(savedToken));
+    }
+
+
+}
--- src/main/java/com/koliving/api/user/IUserService.java
+++ src/main/java/com/koliving/api/user/IUserService.java
@@ -3,8 +3,6 @@ package com.koliving.api.user;
 
 public interface IUserService {
 
-    void saveTokenAndSendEmail(String mail);
-
     User signUp(String email);
 
     void setPassword(User user, String password);
