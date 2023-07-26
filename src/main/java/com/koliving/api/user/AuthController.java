4482d48818b7e30efbda44acf458b19f75794152
noah0504789 <noah0504@naver.com>
2023-07-26 19:43:45 +0900

    Feat: Add signUp method

--- src/main/java/com/koliving/api/user/AuthController.java
+++ src/main/java/com/koliving/api/user/AuthController.java
@@ -1,9 +1,17 @@
 package com.koliving.api.user;
 
+import com.koliving.api.dto.SignUpDto;
+import com.koliving.api.exception.DuplicateResourceException;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.validation.Validator;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,4 +28,17 @@ public class AuthController {
         webDataBinder.addValidators(emailDuplicationValidator);
     }
 
+    @PostMapping("/sign-up")
+    public ResponseEntity signUp(final @Valid @RequestBody SignUpDto signUpDto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            String errorField = bindingResult.getFieldError().getField();
+            String errorCode = bindingResult.getFieldError().getCode();
+            String duplicatedEmail = bindingResult.getFieldError().getDefaultMessage();
+            throw new DuplicateResourceException(errorField + "_" + errorCode + ":" + duplicatedEmail);
+        }
+
+        authFacade.processEmailAuth(signUpDto.email());
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
