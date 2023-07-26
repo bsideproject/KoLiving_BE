9feabc2ee87309049eb14b0d3aa218c79b8b8b3b
noah0504789 <noah0504@naver.com>
2023-07-26 19:02:10 +0900

    Refactor: Import messages that are not in the db table by delegating them to ResourceBundlerMessageSource

--- src/main/java/com/koliving/api/i18n/MessageSource.java
+++ src/main/java/com/koliving/api/i18n/MessageSource.java
@@ -2,22 +2,32 @@ package com.koliving.api.i18n;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.support.AbstractMessageSource;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.stereotype.Component;
 
 import java.text.MessageFormat;
 import java.util.Locale;
 
-@Component("messageSource")
-public class DBMessageSource extends AbstractMessageSource {
+@Component
+public class MessageSource extends AbstractMessageSource {
+
+    @Autowired
+    private ResourceBundleMessageSource resourceBundleMessageSource;
 
     @Autowired
     private LanguageRepository languageRepository;
 
     @Override
     protected MessageFormat resolveCode(String key, Locale locale) {
+        String messageContent;
         Language message = languageRepository.findByLocaleAndMessageKey(locale.toString(), key);
 
-        return new MessageFormat(message.getMessageContent(), locale);
+        if (message == null) {
+            messageContent = resourceBundleMessageSource.getMessage(key, null, locale);
+        } else {
+            messageContent = message.getMessageContent();
+        }
+
+        return new MessageFormat(messageContent, locale);
     }
 }
-
