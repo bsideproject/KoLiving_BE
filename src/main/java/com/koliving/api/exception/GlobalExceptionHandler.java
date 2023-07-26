79b0e63031482713a2be416db1913dc86bec5bbd
noah0504789 <noah0504@naver.com>
2023-07-27 02:11:18 +0900

    Feat: ExceptionHandler - DuplicateResourceException

--- src/main/java/com/koliving/api/exception/GlobalExceptionHandler.java
+++ src/main/java/com/koliving/api/exception/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package com.koliving.api.exception;
 
 import com.koliving.api.dto.ResponseDto;
 import com.koliving.api.dto.ValidationResult;
+import com.koliving.api.i18n.MessageSource;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,9 +10,13 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Locale;
+
 @RestControllerAdvice
 @RequiredArgsConstructor
 public class GlobalExceptionHandler {
+
+    private final MessageSource messageSource;
 
     @ExceptionHandler(value = MethodArgumentNotValidException.class)
     public ResponseEntity<ResponseDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
@@ -21,5 +26,18 @@ public class GlobalExceptionHandler {
         int value = badRequest.value();
 
         return new ResponseEntity<>(ResponseDto.failure(errors ,value), badRequest);
+    }
+
+    @ExceptionHandler(value = DuplicateResourceException.class)
+    public ResponseEntity<ResponseDto> handleEmailDuplicationException(DuplicateResourceException e, Locale locale) {
+        String[] messageKeyAndEmail = e.getMessage().split(":");
+        String messageKey = messageKeyAndEmail[0];
+        String email = messageKeyAndEmail[1];
+        String errorMessage = messageSource.getMessage(messageKey, new Object[]{email}, locale);
+
+        HttpStatus badRequest = HttpStatus.BAD_REQUEST;
+        int value = badRequest.value();
+
+        return new ResponseEntity<>(ResponseDto.failure(errorMessage ,value), badRequest);
     }
 }
