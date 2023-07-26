d9ec25a8c22f7c9609a1ae53501e91b441063bcc
noah0504789 <noah0504@naver.com>
2023-07-26 16:50:36 +0900

    EmailConfig: Injecting property settings bean

--- src/main/java/com/koliving/api/config/EmailConfig.java
+++ src/main/java/com/koliving/api/config/EmailConfig.java
@@ -1,8 +1,7 @@
 package com.koliving.api.config;
 
-import lombok.Getter;
-import lombok.Setter;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import com.koliving.api.properties.EmailProperties;
+import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -10,27 +9,21 @@ import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 import java.util.Properties;
 
-@Getter
-@Setter
+@AllArgsConstructor
 @Configuration
-@ConfigurationProperties(prefix = "spring.mail")
 public class EmailConfig {
 
-    private String host;
-    private int port;
-    private String username;
-    private String password;
+    private final EmailProperties emailProperties;
 
     @Bean
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
-        mailSender.setHost(host);
-        mailSender.setPort(port);
-        mailSender.setUsername(username);
-        mailSender.setPassword(password);
+        mailSender.setHost(emailProperties.getHost());
+        mailSender.setPort(emailProperties.getPort());
+        mailSender.setUsername(emailProperties.getUsername());
+        mailSender.setPassword(emailProperties.getPassword());
 
         Properties props = mailSender.getJavaMailProperties();
-
         props.put("mail.transport.protocol", "smtp");
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.starttls.enable", "true");
