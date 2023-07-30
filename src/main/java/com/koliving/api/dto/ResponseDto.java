2bbcc91bd20eb0068a0582c13b215210edde3703
noah0504789 <noah0504@naver.com>
2023-07-30 22:22:02 +0900

    Refactor: Prohibit setter

--- src/main/java/com/koliving/api/dto/ResponseDto.java
+++ src/main/java/com/koliving/api/dto/ResponseDto.java
@@ -1,27 +1,28 @@
 package com.koliving.api.dto;
 
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-@Setter
-@RequiredArgsConstructor
+
+@Getter
+@NoArgsConstructor
 public class ResponseDto<T> {
 
-    private final int responseCode;
+    private int responseCode;
     private T data;
     private T error;
 
     public static <T> ResponseDto<T> success(T data, int responseCode) {
-        ResponseDto<T> responseDto = new ResponseDto<>(responseCode);
-        responseDto.setData(data);
-        responseDto.setError(null);
+        ResponseDto<T> responseDto = new ResponseDto<>();
+        responseDto.responseCode = responseCode;
+        responseDto.data = data;
         return responseDto;
     }
 
     public static <T> ResponseDto<T> failure(T error, int responseCode) {
-        ResponseDto<T> responseDto = new ResponseDto<>(responseCode);
-        responseDto.setData(null);
-        responseDto.setError(error);
+        ResponseDto<T> responseDto = new ResponseDto<>();
+        responseDto.responseCode = responseCode;
+        responseDto.error = error;
         return responseDto;
     }
 }
