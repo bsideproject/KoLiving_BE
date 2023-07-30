07c1e5241aa08da097f5ec17de600a7d075bf79e
noah0504789 <noah0504@naver.com>
2023-07-31 02:46:07 +0900

    Feature: Add setPassword()

--- src/main/java/com/koliving/api/user/AuthController.java
+++ src/main/java/com/koliving/api/user/AuthController.java
@@ -1,16 +1,21 @@
 package com.koliving.api.user;
 
+import com.koliving.api.dto.SetPasswordDto;
 import com.koliving.api.dto.SignUpDto;
 import com.koliving.api.exception.DuplicateResourceException;
 import com.koliving.api.validation.EmailDuplicationValidator;
+import jakarta.inject.Provider;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindException;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,7 +24,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
 
     private final EmailDuplicationValidator emailDuplicationValidator;
+    private final Provider<UserPropertyEditor> userPropertyEditorProvider;
+    private final UserService userService;
     private final AuthFacade authFacade;
+
+    @InitBinder
+    public void initBinder(WebDataBinder dataBinder){
+        dataBinder.registerCustomEditor(User.class, userPropertyEditorProvider.get());
+    }
 
     @PostMapping("/sign-up")
     public ResponseEntity signUp(final @Valid @RequestBody SignUpDto signUpDto) {
@@ -33,6 +45,13 @@ public class AuthController {
         }
 
         authFacade.processEmailAuth(signUpDto.email());
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PostMapping("/password")
+    public ResponseEntity setPassword(final @Valid @RequestBody SetPasswordDto setPasswordDto, @RequestParam User user) {
+        userService.setPassword(user, setPasswordDto.password());
 
         return new ResponseEntity<>(HttpStatus.OK);
     }
