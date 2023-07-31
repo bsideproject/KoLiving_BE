806afa6caa216509a794471c875227e11a649a67
noah0504789 <noah0504@naver.com>
2023-08-01 05:02:42 +0900

    Refactor: Change class-type to record-type [BlackList]

--- src/main/java/com/koliving/api/token/blacklist/BlackAccessToken.java
+++ src/main/java/com/koliving/api/token/blacklist/BlackAccessToken.java
@@ -1,19 +1,10 @@
 package com.koliving.api.token.blacklist;
 
 import lombok.Builder;
-import lombok.Getter;
 
 import java.util.Date;
 
-@Getter
-public class BlackAccessToken {
+@Builder
+public record BlackAccessToken(String accessToken, Date expirationTime) {
 
-    private String accessToken;
-    private Date expirationTime;
-
-    @Builder
-    public BlackAccessToken(String accessToken, Date expirationTime) {
-        this.accessToken = accessToken;
-        this.expirationTime = expirationTime;
-    }
 }
