e6beef988935a7e231787c7d4c354488e2599a46
noah0504789 <noah0504@naver.com>
2023-08-01 04:32:53 +0900

    Feature: Modify Language's inquiry method for return type using optional

--- src/main/java/com/koliving/api/i18n/LanguageRepository.java
+++ src/main/java/com/koliving/api/i18n/LanguageRepository.java
@@ -4,9 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Repository
 @Transactional(readOnly = true)
-public interface LanguageRepository extends JpaRepository<Language, Integer> {
+public interface LanguageRepository extends JpaRepository<Language, Long> {
 
-    Language findByLocaleAndMessageKey(String locale, String key);
+    Optional<Language> findByLocaleAndMessageKey(String locale, String key);
 }
