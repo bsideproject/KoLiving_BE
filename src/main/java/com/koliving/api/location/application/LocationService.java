229b05535b6537e79b5fdc48ffa153a50c9d7cb3
haedoang <haedoang@gmail.com>
2023-08-13 16:08:49 +0900

    [feature/location] 지역 API

---
+++ src/main/java/com/koliving/api/location/application/LocationService.java
@@ -0,0 +1,65 @@
+package com.koliving.api.location.application;
+
+import static com.koliving.api.base.ServiceError.RECORD_NOT_EXIST;
+
+import com.koliving.api.base.exception.KolivingServiceException;
+import com.koliving.api.location.application.dto.LocationResponse;
+import com.koliving.api.location.application.dto.LocationSaveRequest;
+import com.koliving.api.location.application.dto.LocationUpdateRequest;
+import com.koliving.api.location.domain.Location;
+import com.koliving.api.location.infra.LocationRepository;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class LocationService {
+
+    private final LocationRepository locationRepository;
+
+    @Transactional
+    public Long save(LocationSaveRequest request) {
+        final Location savedLocation = locationRepository.save(request.toEntity(getUpperLocation(request)));
+        return savedLocation.getId();
+    }
+
+    private Location getUpperLocation(LocationSaveRequest request) {
+        if (Objects.isNull(request.upperLocationId())) {
+            return null;
+        }
+        return findById(request.upperLocationId());
+    }
+
+    public LocationResponse findOne(Long id) {
+        final Location location = findById(id);
+        return LocationResponse.valueOf(location);
+    }
+
+    public List<LocationResponse> findAll() {
+        return locationRepository.findAll()
+            .stream()
+            .map(LocationResponse::valueOf)
+            .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void update(Long id, LocationUpdateRequest request) {
+        final Location location = findById(id);
+        location.update(request.enName(), request.krName());
+    }
+
+    @Transactional
+    public void deleteById(Long id) {
+        locationRepository.deleteById(id);
+    }
+
+    private Location findById(Long id) {
+        return locationRepository.findById(id)
+            .orElseThrow(() -> new KolivingServiceException(RECORD_NOT_EXIST));
+    }
+}
---
+++ src/main/java/com/koliving/api/location/domain/Location.java
@@ -0,0 +1,91 @@
+package com.koliving.api.location.domain;
+
+import static com.koliving.api.base.ServiceError.*;
+import static jakarta.persistence.EnumType.STRING;
+import static jakarta.persistence.GenerationType.IDENTITY;
+import static lombok.AccessLevel.PROTECTED;
+
+import com.koliving.api.base.ServiceError;
+import com.koliving.api.base.exception.KolivingServiceException;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import java.util.Objects;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@EqualsAndHashCode(of = "id")
+@NoArgsConstructor(access = PROTECTED)
+@Table(name = "LOCATION")
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, name = "en_name")
+    private String enName;
+
+    @Column(name = "kr_name")
+    private String krName;
+
+    @Column(nullable = false,name = "location_type")
+    @Enumerated(STRING)
+    private LocationType locationType;
+
+    @ManyToOne
+    @JoinColumn(name = "upper_location_id")
+    private Location upperLocation;
+
+    private Location(String enName, String krName, LocationType locationType, Location upperLocation) {
+        validate(locationType, upperLocation);
+        this.enName = Objects.requireNonNull(enName);
+        this.krName = krName;
+        this.locationType = Objects.requireNonNull(locationType);
+        this.upperLocation = upperLocation;
+    }
+
+    //TODO 예외 코드화하기
+    private void validate(LocationType locationType, Location upperLocation) {
+        if (locationType.isRequiredUpperLocation() && Objects.isNull(upperLocation)) {
+            throw new KolivingServiceException(INVALID_LOCATION);
+        }
+
+        if (locationType.isTopLocation() && Objects.nonNull(upperLocation)) {
+            throw new KolivingServiceException(INVALID_LOCATION);
+        }
+    }
+
+    public static Location valueOf(String enName, String krName, LocationType locationType) {
+        return new Location(enName, krName, locationType, null);
+    }
+
+    public static Location valueOf(String enName, String krName, LocationType locationType, Location upperLocation) {
+        return new Location(enName, krName, locationType, upperLocation);
+    }
+
+    public String displayName() {
+        return String.format("%s-%s", enName, locationType.getEnName());
+    }
+
+    public Long getUpperLocationId() {
+        if (Objects.isNull(upperLocation)) {
+            return null;
+        }
+
+        return upperLocation.id;
+    }
+
+    public void update(String enName, String krName) {
+        this.enName = Objects.requireNonNull(enName);
+        this.krName = krName;
+    }
+}
