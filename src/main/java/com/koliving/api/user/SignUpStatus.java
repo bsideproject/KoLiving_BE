bcdcb6907f8abd1b0f3ac8b34a8b006bf659dce2
noah0504789 <noah0504@naver.com>
2023-08-26 02:06:09 +0900

    feat: add getRedirectUri() to SignUpStatus

--- src/main/java/com/koliving/api/user/SignUpStatus.java
+++ src/main/java/com/koliving/api/user/SignUpStatus.java
@@ -1,7 +1,27 @@
 package com.koliving.api.user;
 
+import com.koliving.api.utils.HttpUtils;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
+
+@Getter
 public enum SignUpStatus {
-    PASSWORD_VERIFICATION_PENDING,
-    PROFILE_INFORMATION_PENDING,
-    COMPLETED
+
+    PASSWORD_VERIFICATION_PENDING("/password"), PROFILE_INFORMATION_PENDING("/profile"), COMPLETED("/login");
+
+    private final String redirectResourcePath;
+
+    SignUpStatus(String redirectResourcePath) {
+        this.redirectResourcePath = redirectResourcePath;
+    }
+
+    public String getRedirectUri(HttpUtils httpUtils, HttpServletRequest request) {
+        if (this == PASSWORD_VERIFICATION_PENDING || this == PROFILE_INFORMATION_PENDING) {
+            return httpUtils.getRedirectUri(request, getRedirectResourcePath());
+        } else if (this == COMPLETED) {
+            return httpUtils.getCurrentVersionUri(getRedirectResourcePath());
+        }
+
+        return null;
+    }
 }
