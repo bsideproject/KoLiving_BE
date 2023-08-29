6421a0b01d096f653d98efb005ea920c0a33f31a
noah0504789 <noah0504@naver.com>
2023-08-30 04:54:08 +0900

    feat: caching function that returns constant value with gender code

--- src/main/java/com/koliving/api/user/Gender.java
+++ src/main/java/com/koliving/api/user/Gender.java
@@ -2,6 +2,9 @@ package com.koliving.api.user;
 
 import lombok.Getter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Getter
 public enum Gender {
     MALE(0), FEMALE(1), OTHER(2);
@@ -10,5 +13,17 @@ public enum Gender {
 
     Gender(int code) {
         this.code = code;
+    }
+
+    private final static Map<Integer, Gender> map;
+    static {
+        map = new HashMap<>();
+        for (Gender gender : Gender.values()) {
+            map.put(gender.getCode(), gender);
+        }
+    }
+
+    public static Gender findByCode(int code) {
+        return map.get(code);
     }
 }
