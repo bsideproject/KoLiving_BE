f094efcd9265fb5c839e8e3001f24d38113e2a6f
noah0504789 <noah0504@naver.com>
2023-08-31 19:06:42 +0900

    refactor: remote getRedirectUri()

--- src/main/java/com/koliving/api/user/SignUpStatus.java
+++ src/main/java/com/koliving/api/user/SignUpStatus.java
@@ -1,7 +1,5 @@
 package com.koliving.api.user;
 
-import com.koliving.api.utils.HttpUtils;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.Getter;
 
 @Getter
@@ -15,15 +13,5 @@ public enum SignUpStatus {
 
     SignUpStatus(String redirectPath) {
         this.redirectPath = redirectPath;
-    }
-
-    public String getRedirectUri(HttpUtils httpUtils, HttpServletRequest request) {
-        if (this == PASSWORD_VERIFICATION_PENDING || this == PROFILE_INFORMATION_PENDING) {
-            return httpUtils.getRedirectUri(request, getRedirectPath());
-        } else if (this == COMPLETED) {
-            return httpUtils.getCurrentVersionPath(getRedirectPath());
-        }
-
-        return null;
     }
 }
