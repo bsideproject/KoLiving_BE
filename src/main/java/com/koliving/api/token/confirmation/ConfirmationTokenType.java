594f9b11460dcbdc3b18123068837e3228ab121d
noah0504789 <noah0504@naver.com>
2023-09-04 16:30:04 +0900

    refactor: fix reset-password redirect path

--- src/main/java/com/koliving/api/token/confirmation/ConfirmationTokenType.java
+++ src/main/java/com/koliving/api/token/confirmation/ConfirmationTokenType.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 @Getter
 public enum ConfirmationTokenType {
     SIGN_UP("auth/sign-up/confirm", "/signup/step2"),
-    RESET_PASSWORD("auth/reset-password/confirm", "/resetPassword/step2");
+    RESET_PASSWORD("auth/reset-password/confirm", "/resetPassword/step1");
 
     private final String linkPathResource;
     private final String redirectPath;
