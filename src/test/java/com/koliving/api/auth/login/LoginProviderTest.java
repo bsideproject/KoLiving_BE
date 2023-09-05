e725f5b0b2ee6ff4ddde4616088b610760c76bb0
noah0504789 <noah0504@naver.com>
2023-09-05 20:59:04 +0900

    feat: test authentication_failure()

--- src/test/java/com/koliving/api/auth/login/LoginProviderTest.java
+++ src/test/java/com/koliving/api/auth/login/LoginProviderTest.java
@@ -8,12 +8,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 import static com.koliving.api.user.UserUtils.createAuthentication;
 import static com.koliving.api.user.UserUtils.createUser;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -40,5 +43,34 @@ class LoginProviderTest {
 
         assertNotNull(authentication);
         assertEquals(dummyEmail, authentication.getName());
+    }
+
+    @Test
+    @DisplayName("authenticate() 실패 : 존재하지 않는 이메일")
+    void authenticate_failure_non_exists_email() {
+        String nonExistsEmail = "noAtMark";
+        String dummyPassword = "KolivingPwd12";
+
+        when(userService.loadUserByUsername(nonExistsEmail)).thenThrow(UsernameNotFoundException.class);
+        Authentication auth = createAuthentication(nonExistsEmail, dummyPassword);
+
+        assertThrows(UsernameNotFoundException.class, () -> loginProvider.authenticate(auth));
+    }
+
+    @Test
+    @DisplayName("authenticate() 실패 : 일치하지 않는 패스워드")
+    void authenticate_failure_mismatched_password() {
+        String dummyEmail = "test@koliving.com";
+        String dummyPassword = "KolivingPwd12";
+        User dummyUser = createUser(dummyEmail, dummyPassword);
+
+        when(userService.loadUserByUsername(dummyEmail)).thenReturn(dummyUser);
+
+        String mismatchedPassword = "not_equal_password";
+        when(userService.isEqualPassword(mismatchedPassword, dummyUser.getPassword())).thenReturn(false);
+
+        Authentication auth = createAuthentication(dummyEmail, mismatchedPassword);
+
+        assertThrows(BadCredentialsException.class, () -> loginProvider.authenticate(auth));
     }
 }
